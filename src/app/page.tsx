22dd7689a07ea35e219fe0d69c223b34f24ec478
Hamza Khan <m.hamzakhan91able@gmail.com>
2023-05-15 18:48:09 +0500

    Added analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { Analytics } from "@vercel/analytics/react";
+
 import Footer from "./Components/Footer/Footer";
 import Header from "./Components/Header/Header";
 import About from "./Pages/About/About";
@@ -17,6 +19,7 @@ export default function Home() {
         <Testimonials />
         <Footer />
       </Scroll>
+      <Analytics />
     </>
   );
 }
