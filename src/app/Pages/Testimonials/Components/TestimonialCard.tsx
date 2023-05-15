0003ee1de4cbd5acd8fd4151b62d0e53ae7575a3
Hamza Khan <m.hamzakhan91able@gmail.com>
2023-05-15 16:55:21 +0500

    Todo

--- src/app/Pages/Testimonials/Components/TestimonialCard.tsx
+++ src/app/Pages/Testimonials/Components/TestimonialCard.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import React from "react";
 
+// TODO: Make it dynamic
 export default function TestimonialCard() {
   return (
     <section className="bg-white dark:bg-gray-900">
